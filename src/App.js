b64dc6f79f09ed617902a7b30b4dcf865b1da3fa
meshacklee <meshacklee.com@gmail.com>
2024-12-20 16:30:16 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          under Construction happy christmas
+          under Construction happy christmas from EC petrol 
         </p>
        
       </header>

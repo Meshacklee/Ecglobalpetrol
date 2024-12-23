34806612ccd54877344dfdad4532a937f1dee5f8
meshacklee <meshacklee.com@gmail.com>
2024-12-23 10:18:26 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import './index.css';
+import { Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          under Construction happy merry christmas from EC petrol 
-        </p>
-       
-      </header>
-    </div>
+    <routes>
+    
+    </routes>
   );
 }
 
---
+++ src/Components/Nav.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className="secondary-nav">
+            <div className="container">
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                  <a className="navbar-brand" href="#">ECGLOBALPETROL</a>
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto">
+                      <li className="nav-item">
+                        <a className="nav-link" aria-current="page" href="/">HOME</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="/Catalog">ABOUT-US</a>
+                      </li>
+                      <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        PRODUCTS
+                        </a>
+                        <ul className="dropdown-menu">
+                          <li><a className="dropdown-item" href="#">PETROL-STATION</a></li>
+                          <li><a className="dropdown-item" href="#">REAL-ESTATE</a></li>
+                          <li><hr className="dropdown-divider"/></li>
+                          <li><a className="dropdown-item" href="#">OTHERS</a></li>
+                        </ul>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="/Collection">PROFILE</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="/Blog">BLOG</a>
+                      </li>
+                    </ul>
+      
+                  </div>
+                </div>
+              </nav>
+
+
+        </div>  
+        </div>
+  )
+}
+
+export default Nav
